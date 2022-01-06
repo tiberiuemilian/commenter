b5b04ae0fe39f5ad7c945bf03f43d608582dde1b
Tiberiu Emilian Jalba <tiberiuemilian@gmail.com>
2022-01-06 12:45:00 +0200

    Boostrap the project with Spring Boot Initializr

---
+++ src/main/java/com/example/commenter/CommenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.commenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commenter/CommenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
